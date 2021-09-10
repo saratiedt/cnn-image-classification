e3186aeb43ae7b9a09e490099cbbbc4754b6f663
saratiedt <saracristinetiedt@gmail.com>
2021-09-10 10:50:33 -0300

    adiucionado novos imports

--- cnn.py
+++ cnn.py
@@ -1,10 +1,13 @@
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-from keras.layers.normalization import BatchNormalization
-from keras.preprocessing.image import ImageDataGenerator
+from tensorflow import keras
+from tensorflow.keras import Sequential
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 import numpy as np
-from keras.preprocessing import image
+from tensorflow.keras.preprocessing import image
+
 
 # defininso o RNA
 model = Sequential()
