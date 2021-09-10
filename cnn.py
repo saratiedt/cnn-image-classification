b7312200908b5826a4e5bdad88aa296751e97d01
saratiedt <saracristinetiedt@gmail.com>
2021-09-10 11:47:27 -0300

    configurações

--- cnn.py
+++ cnn.py
@@ -9,35 +9,47 @@ import numpy as np
 from tensorflow.keras.preprocessing import image
 
 
+
 # defininso o RNA
 model = Sequential()
 
+# função relu transforma tudo que é menor que zero pra zero
 model.add(Conv2D(32, (3, 3), input_shape=(64, 64, 3), activation='relu'))
 
-#pega o mapa de caracteristicas e normaliza os valores entre 0 e 1
+# pega o mapa de caracteristicas e normaliza os valores entre 0 e 1
 model.add(BatchNormalization())
 
-#matriz do max pooling com as principais caracteristicas matrix 2x2
+# matriz do max pooling com as principais caracteristicas matrix 2x2
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-#adicionando mais uma camada sw convolução
+# adicionando mais uma camada de convolução
 model.add(Conv2D(32, (3, 3), input_shape=(64, 64, 3), activation='relu'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-#altera a estrutura de dados para um vetor
+# altera a estrutura de dados para um vetor
 model.add(Flatten())
+
+# configuração da rede neural com 2 camadas ocultas
+# adiciona a primeira camada oculta com 128 neuronios e 128 caracteristicas
+# cada imagem tem 64x64 pixels de dimensão, cada pixel possui uma caracteristica
 model.add(Dense(units=128, activation='relu'))
+
+# o dropout para zerar algumas entradas da camada oculta, neste caso 20%
 model.add(Dropout(0.2))
+
+# criando mais uma camada oculta
 model.add(Dense(units=128, activation='relu'))
 model.add(Dropout(0.2))
 
+# criando camada de saída da RNA
 model.add(Dense(units=1, activation='sigmoid'))
 
-
+# configurando atributos para o treinamento
 model.compile(optimizer='adam', loss='binary_crossentropy',
               metrics=['accuracy'])
 
+# carregando dados do treinamento
 training_generate = ImageDataGenerator(rescale=1/255,
                                        rotation_range=7,
                                        horizontal_flip=True,
